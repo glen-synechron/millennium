b00a557a56f919587f6e31077d8fc51924f2111e
Glen Noronha <glen.noronha@syncheron.com>
2013-01-10 15:46:21 +0530

    Removed the millennium class in the models folder. Specifying the css assets to precompile.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,7 @@ BeautyBookMillennium::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
+  config.assets.precompile += %w( common.css datePicker.css *.js )
 
   # Generate digests for assets URLs
   config.assets.digest = true
